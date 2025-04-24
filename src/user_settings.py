bfb950dfb0fd2fe9cc50beae9103841eb9fbf163
mahdi021zamani <167538602+mahdi021zamani@users.noreply.github.com>
2025-04-24 10:14:15 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -5,22 +5,10 @@
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
     "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
-    "https://t.me/s/v2rayfree",
-    "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/meli_proxyy",
-    "https://t.me/s/DirectVPN",
-    "https://t.me/s/VmessProtocol",
-    "https://t.me/s/heyatserver",
-    "https://t.me/s/vpnfail_vless",
-    "https://t.me/s/DailyV2RY",
-    "https://t.me/s/shadowproxy66",
-    "https://t.me/s/OutlineReleasedKey",
-    "https://t.me/s/proxy_shadosocks",
-    "https://t.me/s/ViProxys",
+    "https://vpn.fail/free-proxy/v2ray#v2ray",
+    "https://shadowmere.xyz/api/b64sub/",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt#V2RAY_RAW",
+  
     # Add more URLs here if you want to include additional sources.
 ]
 
